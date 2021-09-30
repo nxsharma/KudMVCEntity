82619cffb5ca3601cad244fdea6da0aa987b10a5
Nikhil Sharma <nikhil.sharma@northcare.com>
2021-09-30 10:56:03 -0500

    first commit

---
+++ KudMVCEntityDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KudMVCEntityDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
